7df809753a38da971268a4a95acc1dc7587578dc
尘息 <chenxijun2247338@hotmail.com>
2020-04-20 10:13:30 +0800

    Edit

--- pages/about.md
+++ pages/about.md
@@ -3,30 +3,20 @@ layout: mypost
 title: 关于我
 ---
 
-> Hello 陌生人，欢迎访问 TMaize Blog
+> `Hello, World!`
 
-该博客托管于 GitHub Page，国内默认解析到腾讯云，以保证国内外的访问速度。留言页面使用腾讯的“吐个槽”，另外使用的腾讯的 MTA 分析工具统计访问量
+## 我是谁
 
-主题使自己写的，见[tmaize-blog](https://github.com/TMaize/tmaize-blog)，欢迎 Star&Fork。另喜欢本博客的欢迎**添加友链**，在[留言板](chat.html)留言即可
+- 了解 Python, C#, C/C++ 等语言，对计算机有着浓厚的兴趣
 
-## 相关技能
+- 退役OIer，是个只拿了二等的蒟蒻
 
-- 熟悉 JavaScript 语言， 熟练使用 Vue 全家桶,jQuery 等前端框架
+- 爱党、爱国的热心公民
 
-- 熟悉 Java，NodeJs,Go 等后端语言
+- 热爱吹水（装逼）、辩论（撕逼）
 
-- 后端框架 Spring，Hibernate，JFinal，Mybatis 略有了解
-
-- 数据库方面能熟练使用 MySQL,Oracle，MongoDB
-
-- Linux 的简单使用，各种服务的搭建
-
-- 能够使用 Git/SVN 对代码版本进行控制
+- 一名二线城市的高中生
 
 ## 联系我
 
-- QQ : 1772314831
-
-- Email : [tmaize@qq.com](http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=YBQNAQkaBSAREU4DDw0)
-
-- GitHub : [https://github.com/tmaize](https://github.com/tmaize)
+- GitHub : [https://github.com/chenxijun](https://github.com/chenxijun)
--- static/js/blog.js
+++ static/js/blog.js
@@ -198,54 +198,54 @@ blog.ajax = function(option, success, fail) {
 /**
  * 特效：点击页面文字冒出特效
  */
-// blog.initClickEffect = function(textArr) {
-//   function createDOM(text) {
-//     var dom = document.createElement('span')
-//     dom.innerText = text
-//     dom.style.left = 0
-//     dom.style.top = 0
-//     dom.style.position = 'fixed'
-//     dom.style.fontSize = '12px'
-//     dom.style.whiteSpace = 'nowrap'
-//     dom.style.webkitUserSelect = 'none'
-//     dom.style.userSelect = 'none'
-//     dom.style.opacity = 0
-//     dom.style.transform = 'translateY(0)'
-//     dom.style.webkitTransform = 'translateY(0)'
-//     return dom
-//   }
+blog.initClickEffect = function(textArr) {
+  function createDOM(text) {
+    var dom = document.createElement('span')
+    dom.innerText = text
+    dom.style.left = 0
+    dom.style.top = 0
+    dom.style.position = 'fixed'
+    dom.style.fontSize = '12px'
+    dom.style.whiteSpace = 'nowrap'
+    dom.style.webkitUserSelect = 'none'
+    dom.style.userSelect = 'none'
+    dom.style.opacity = 0
+    dom.style.transform = 'translateY(0)'
+    dom.style.webkitTransform = 'translateY(0)'
+    return dom
+  }
 
-//   blog.addEvent(window, 'click', function(ev) {
-//     var tagName = ev.target.tagName.toLocaleLowerCase()
-//     if (tagName == 'a') {
-//       return
-//     }
-//     var text = textArr[parseInt(Math.random() * textArr.length)]
-//     var dom = createDOM(text)
+  blog.addEvent(window, 'click', function(ev) {
+    var tagName = ev.target.tagName.toLocaleLowerCase()
+    if (tagName == 'a') {
+      return
+    }
+    var text = textArr[parseInt(Math.random() * textArr.length)]
+    var dom = createDOM(text)
 
-//     document.body.appendChild(dom)
-//     var w = parseInt(window.getComputedStyle(dom, null).getPropertyValue('width'))
-//     var h = parseInt(window.getComputedStyle(dom, null).getPropertyValue('height'))
+    document.body.appendChild(dom)
+    var w = parseInt(window.getComputedStyle(dom, null).getPropertyValue('width'))
+    var h = parseInt(window.getComputedStyle(dom, null).getPropertyValue('height'))
 
-//     var sh = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0
-//     dom.style.left = ev.pageX - w / 2 + 'px'
-//     dom.style.top = ev.pageY - sh - h + 'px'
-//     dom.style.opacity = 1
+    var sh = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0
+    dom.style.left = ev.pageX - w / 2 + 'px'
+    dom.style.top = ev.pageY - sh - h + 'px'
+    dom.style.opacity = 1
 
-//     setTimeout(function() {
-//       dom.style.transition = 'transform 500ms ease-out, opacity 500ms ease-out'
-//       dom.style.webkitTransition = 'transform 500ms ease-out, opacity 500ms ease-out'
-//       dom.style.opacity = 0
-//       dom.style.transform = 'translateY(-26px)'
-//       dom.style.webkitTransform = 'translateY(-26px)'
-//     }, 20)
+    setTimeout(function() {
+      dom.style.transition = 'transform 500ms ease-out, opacity 500ms ease-out'
+      dom.style.webkitTransition = 'transform 500ms ease-out, opacity 500ms ease-out'
+      dom.style.opacity = 0
+      dom.style.transform = 'translateY(-26px)'
+      dom.style.webkitTransform = 'translateY(-26px)'
+    }, 20)
 
-//     setTimeout(function() {
-//       document.body.removeChild(dom)
-//       dom = null
-//     }, 520)
-//   })
-// }
+    setTimeout(function() {
+      document.body.removeChild(dom)
+      dom = null
+    }, 520)
+  })
+}
 
 // 新建DIV包裹TABLE
 blog.addLoadEvent(function() {
